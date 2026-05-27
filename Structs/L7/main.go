ad099975de2b15ca4b889227941915f9c9162ffe
Joseph Pendon <pendon.josephjr@gmail.com>
2026-05-27 23:04:26 +0800

    submit L7

---
+++ Structs/L7/main.go
@@ -0,0 +1,32 @@
+package main
+
+// ?
+func (u User) SendMessage(message string, messageLenght int) (string, bool) {
+	if messageLenght <= u.MessageCharLimit {
+		return message, true
+	}
+	return "", false
+}
+
+// don't touch below this line
+
+type User struct {
+	Name string
+	Membership
+}
+
+type Membership struct {
+	Type             string
+	MessageCharLimit int
+}
+
+func newUser(name string, membershipType string) User {
+	membership := Membership{Type: membershipType}
+	if membershipType == "premium" {
+		membership.MessageCharLimit = 1000
+	} else {
+		membership.Type = "standard"
+		membership.MessageCharLimit = 100
+	}
+	return User{Name: name, Membership: membership}
+}
